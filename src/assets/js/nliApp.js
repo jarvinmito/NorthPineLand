4222797c0c8005fe824cfaee5102a525417eb909
jarvinm <mito.jan_arvin@yahoo.com>
2016-06-15 14:39:57 +0800

    update: calculator on dashboard

--- src/assets/js/nliApp.js
+++ src/assets/js/nliApp.js
@@ -1,28 +1,34 @@
 var nliApp = (function(){
 
-	var initDataTables = function(){
-		if( $('.yondu-table').length ){
-			$('.yondu-table').each(function(){
-				var curr = $(this);
-				var options = {
-					"oLanguage" : {
-						"oPaginate" : {
-							"sNext" : "&gt;",
-							"sLast" : "&gt;&gt;",
-							"sFirst" : "&lt;&lt;",
-							"sPrevious" : "&lt;"
-						},
-						"paging" : false
-					}
-				};
-				
-				if( curr.data('has-scroll') ){
-					options.scrollX = true;
-				}
+	var initDataTables = function(special){
+		var options = {
+			"oLanguage" : {
+				"oPaginate" : {
+					"sNext" : "&gt;",
+					"sLast" : "&gt;&gt;",
+					"sFirst" : "&lt;&lt;",
+					"sPrevious" : "&lt;"
+				},
+				"paging" : false
+			}
+		};
 
-				$(this).dataTable(options);
-			});
+		if( !special ){
+			if( $('.yondu-table').length ){
+				$('.yondu-table').each(function(){
+					var curr = $(this);
+					
+					if( curr.data('has-scroll') ){
+						options.scrollX = true;
+					}
+
+					$(this).dataTable(options);
+				});
+			}
+		}else{
+			$(special).dataTable(options);
 		}
+
 	};
 
 
@@ -218,9 +224,65 @@ var nliApp = (function(){
 
 	var initLotModal = function(){
 		var modal = $('#lotsModal');
-		$(document).on('click', '[data-is-lot]', function(e){
+
+		var renderList = function(data){
+			var mhtml = modal.find('.yondu-modal__data');
+			var filtercontainer = mhtml.find('.filter');
+			var tablecontainer = mhtml.find('.table');
+			var container = $('<div class="yondu-container row" />');
+			
+			var table = $("<table class=\"yondu-table available-lots\" />");
+
+			var columns = data.project.lots.columns;
+			var records = data.project.lots.records;
+			
+			var tablehtml = "<thead>";
+			tablehtml += "<tr>";
+			for( var column in columns){
+				tablehtml += "<th>"+columns[column]+"</th>";
+			}
+			tablehtml += "<tr>";
+			tablehtml += "</tr>";
+			tablehtml += "</thead>";
+			tablehtml += "<tbody>";
+			for( var record in records ){
+				tablehtml += "<tr>";
+					var rcols = columns;
+					var item = records[record];
+					for( var rcol in rcols){
+						tablehtml += "<td>"+item[rcols[rcol]]+"</td>";
+					}
+				tablehtml += "</tr>";
+			}
+			tablehtml += "</tbody>";
+			tablehtml += "</table>";
+
+
+			table.html(tablehtml);
+			container.html(table);
+			tablecontainer.html(container);
+
+			$('.yondu-table.available-lots').dataTable();
+		};
+
+
+
+		$(document).on('click', '.yondu-table__buttons [data-is-lot]', function(e){
 			e.preventDefault();
-			modal.foundation('open');
+			var url = "/available-lots.json";
+			$.ajax({
+				url : url,
+				beforeSend : function(xhr){
+					initLoader().show();
+				},
+				success : function(res){
+					// Fill the modal with data
+					console.log(res);
+					renderList(res);
+					initLoader().hide();
+					modal.foundation('open');
+				}
+			})
 		});
 	};
 
@@ -240,60 +302,156 @@ var nliApp = (function(){
 
 				var calcBalance = function(){
 					var toFill = $('#nli-balance');
-					var data = parseInt(downpayment.val());
+					var data = parseFloat(downpayment.val());
 					var balance = (data <= 100) ? 100 - data : 0;
 					toFill.text(balance+"%");
+					// add values to hidden input fields also
+					toFill.next('input[type="hidden"]').val(balance);
 				};
 
 				var calcNetListPrice = function(){
 					var netlistprice = $('#nli-net-list-price');
 					var netcontractprice = $('#nli-net-contract-price');
-					var pd = parseInt(promodiscount.val()) || 0;
-					var tp = parseInt(totalprice.val()) || 0;
+					var pd = parseFloat(promodiscount.val()) || 0;
+					var tp = parseFloat(totalprice.val()) || 0;
 					var computed = tp - pd;
 					netlistprice.text(computed.formatMoney(2,'.',','));
 					netcontractprice.text(computed.formatMoney(2,'.',','));
+					// add values to hidden input fields also
+					netlistprice.next('input[type="hidden"]').val(computed);
+					netcontractprice.next('input[type="hidden"]').val(computed);
 				};
 
 				var calcTotalDiscounts = function(){
 					var totaldiscount = $('#nli-total-discount');
 					var netdiscount = $('#nli-net-discount');
-					var pd = parseInt(promodiscount.val()) || 0;
-					var pt = parseInt(paytermpromo.val()) || 0;
+					var pd = parseFloat(promodiscount.val()) || 0;
+					var pt = parseFloat(paytermpromo.val()) || 0;
 					var computed = pd + pt;
 					totaldiscount.text(computed.formatMoney(2,'.',','));
 					netdiscount.text(computed.formatMoney(2,'.',','));
+					// add values to hidden input fields also
+					totaldiscount.next('input[type="hidden"]').val(computed);
+					netdiscount.next('input[type="hidden"]').val(computed);
 				};
 
 				var calcNetContractPrice = function(){
 					var netprice = $('#nli-net-contract-price');
-					var pd = parseInt(promodiscount.val()) || 0;
-					var tp = parseInt(totalprice.val()) || 0;
-					var nlp = parseInt() || 0;
+					var pd = parseFloat(promodiscount.val()) || 0;
+					var tp = parseFloat(totalprice.val()) || 0;
+					var nlp = parseFloat() || 0;
 					// var computed = pd + nlp;
-					var computed = parseInt($('#nli-net-list-price').val) || 0;
+					var computed = parseFloat($('#nli-net-list-price').val()) || 0;
 
 					netprice.text(computed.formatMoney(2,'.',','));
+					// add values to hidden input fields also
+					netprice.next('input[type="hidden"]').val(computed);
 				};
 
 				var calcListPriceAllDiscount = function(){
 					var listnetdiscount = $('#nli-list-net-discount');
 					var totalsellingprice = $('#nli-total-selling-price');
-					var pd = parseInt(promodiscount.val()) || 0;
-					var pt = parseInt(paytermpromo.val()) || 0;
+					var pd = parseFloat(promodiscount.val()) || 0;
+					var pt = parseFloat(paytermpromo.val()) || 0;
 					var computed = pd + pt;
-					var lpad = parseInt(totalprice.val()) - computed;
+					var lpad = parseFloat(totalprice.val()) - computed;
 
 					listnetdiscount.text(lpad.formatMoney(2,'.',','));
 					totalsellingprice.text(lpad.formatMoney(2,'.',','));
+
+					// add values to hidden input fields also
+					listnetdiscount.next('input[type="hidden"]').val(lpad);
+					totalsellingprice.next('input[type="hidden"]').val(lpad);
 				};
 
-				$('input').on('keyup', function(e){
+				var pmt = function(rate_per_period, number_of_payments, present_value, future_value, type){
+			    if(rate_per_period != 0.0){
+			        // Interest rate exists
+			        var q = Math.pow(1 + rate_per_period, number_of_payments);
+			        return -(rate_per_period * (future_value + (q * present_value))) / ((-1 + q) * (1 + rate_per_period * (type)));
+
+			    } else if(number_of_payments != 0.0){
+			        // No interest rate, but number of payments exists
+			        return -(future_value + present_value) / number_of_payments;
+			    }
+
+			    return 0;
+				};
+
+				var calcAmortization = function(){
+					var pt = parseFloat(prefferedterm.val()) || 0;
+					var pd = parseFloat(promodiscount.val()) || 0;
+					var br = parseFloat(bankrate.val()) || 0;
+					var total = parseFloat(totalprice.val()) || 0;
+					var tsellprice = ( total - ( pd + pt ));
+					var ptdp = parseFloat(downpayment.val()) || 0;
+					var rbal = tsellprice * ( ptdp / 100 );
+
+					// rbal/((1/br)-(1/(br(1+br)^pt)))
+ 					// Formula
+ 					// Converts bank rate percent to decimal e.g. 15% --> 0.15
+ 					// bank rate is divided by the number of months in a year
+ 					// payment term -- in years -- is divided by the number of months in a year
+					var monthly_amortization = -pmt(((br / 100) / 12),(pt * 12),rbal,0,0);// pt /(( br / 12 ) * (rbal));
+
+					// var obj = {
+					// 	payterm : pt,
+					// 	promodiscount : pd,
+					// 	bankrate : br,
+					// 	totalprice : total,
+					// 	totalsellingprice : tsellprice,
+					// 	ptdownpayment : ptdp,
+					// 	requiredbal : rbal,
+					// 	monthlyamor : monthly_amortization
+					// };
+
+					// console.log( obj );
+
+					var amort = $('#nli-monthly-amortization');
+					amort.text(monthly_amortization.formatMoney(2,'.',','));
+
+					// add values to hidden input fields also
+					amort.next('input[type="hidden"]').val(monthly_amortization);
+				};
+
+				var clearForm = function(){
+					currForm.find('input').val('');
+					$('.yondu-forms__data#nli-balance').text('100%');
+					$('.yondu-forms__data.currency').text(parseInt(0).formatMoney(2,'.',','));
+				};
+
+				currForm.on('keyup', 'input', function(e){
 					calcBalance();
 					calcNetListPrice();
 					calcTotalDiscounts();
 					calcListPriceAllDiscount();
+					calcAmortization();
 				});
+
+				currForm.on('change', 'select', function(e){
+					calcBalance();
+					calcNetListPrice();
+					calcTotalDiscounts();
+					calcListPriceAllDiscount();
+					calcAmortization();
+				});
+
+				currForm.on('click', '[data-button-type]' , function(e){
+					e.preventDefault();
+					var currBtn = $(this);
+					var types = {
+						"reset" : function(){
+							clearForm();
+						},
+						"action" : function(){
+							$('.yondu-calculator__form').submit();
+						}
+					};
+
+					console.log(types, currBtn, currBtn.data('button-type'));
+
+					types[currBtn.data('button-type')]();
+				})
 
 				// downpayment.on('keyup', function(e){
 				// 	calcBalance();
